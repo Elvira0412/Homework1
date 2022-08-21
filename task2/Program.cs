1b025e8a23897e3f5315ce197a91a712b3230ee3
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-22 00:39:01 +0300

    Написали решение для задачи №2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход 3 числа и выдает max из этих чисел.
+int userNumber1=new int();
+int userNumber2=new int();
+int userNumber3=new int();
+Console.Write("Введите три числа:");
+userNumber1=Convert.ToInt32(Console.ReadLine());
+userNumber2=Convert.ToInt32(Console.ReadLine());
+userNumber3=Convert.ToInt32(Console.ReadLine());
+if (userNumber1>userNumber2 & userNumber1>userNumber3) Console.WriteLine("Max="+ userNumber1);
+else if (userNumber2>userNumber3) Console.WriteLine("Max="+ userNumber2);
+else Console.WriteLine("Max="+ userNumber3);
