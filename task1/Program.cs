9f4da88b788e9d483896fcaee1be367bc28addd5
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-21 10:17:00 +0300

    Написали программу для Задачи №1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int userNumber1=new int();
+int userNumber2=new int();
+Console.Write("Введите два числа:");
+userNumber1=Convert.ToInt32(Console.ReadLine());
+userNumber2=Convert.ToInt32(Console.ReadLine());
+if (userNumber1>userNumber2) Console.WriteLine($"Max={userNumber1}; Min= {userNumber2}");
+else Console.WriteLine($"Max={userNumber2}; Min= {userNumber1}"); 
