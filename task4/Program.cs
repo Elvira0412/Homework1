efe9bd709bbea184b44e6b95beb768769e656305
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-22 01:11:04 +0300

    Написали программу  для задачи №4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На вход принимает число N , на выходе показывает все четные числа от 1 до N
+int N=new int();
+Console.Write("Введите число N:");
+N=Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<= N; i++);
+{
+    if (i%2==0) Console.WriteLine(i);
+    else i++;
+}
