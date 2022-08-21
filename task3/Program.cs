ef79a80aa089c21c892b3067b8e9ae194deee7a0
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-22 00:53:05 +0300

    Написали программу  для задачи №3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На вход принимает число и выдает,является ли число четным (делится ли оно на два без остатка)
+int userNumber1=new int();
+Console.Write("Введите число:");
+userNumber1=Convert.ToInt32(Console.ReadLine());
+if (userNumber1%2==0) Console.WriteLine($"Число {userNumber1} четное");
+else Console.WriteLine($"Число { userNumber1} нечетное");
+
