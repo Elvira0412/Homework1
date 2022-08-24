c8ad7facd9585664bf8ff01b2ceefcc05627e663
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-24 23:24:47 +0300

    Дописали программу  для задачи №4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,9 +1,10 @@
 ﻿// На вход принимает число N , на выходе показывает все четные числа от 1 до N
-int N=new int();
+
 Console.Write("Введите число N:");
-N=Convert.ToInt32(Console.ReadLine());
-for (int i=1; i<= N; i++);
-{
-    if (i%2==0) Console.WriteLine(i);
-    else i++;
-}
+int N=Convert.ToInt32(Console.ReadLine());
+int i=0;
+while (i < N) 
+Console.WriteLine(i += 2);
+
+
+
